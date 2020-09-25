981fb454e333827007327f6e1bcb361d00902725
Prince <princeagrawal024@gmail.com>
2020-09-25 16:55:18 +0530

    Added Zuul Proxy Server, now all the request will redirected from Zull Proxy Server, based on our url...

--- ProductService/src/main/java/com/springcloud/productservice/restclient/CouponClient.java
+++ ProductService/src/main/java/com/springcloud/productservice/restclient/CouponClient.java
@@ -1,6 +1,5 @@
 package com.springcloud.productservice.restclient;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +7,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springcloud.productservice.model.Coupon;
 
-@FeignClient("COUPON-SERVICE")  //-->Imp1
-@RibbonClient("COUPON-SERVICE") 
+
+@FeignClient("zuul-api-gateway")  
 public interface CouponClient {
-    @RequestMapping(value = "/couponapi/coupons/{code}", method = RequestMethod.GET)
+    @RequestMapping(value = "/coupon-service/couponapi/coupons/{code}", method = RequestMethod.GET)
 	public Coupon getCoupon(@PathVariable("code") String code);//-->Imp2 Rest things exact same except body
 	
 }
+
+
 
 /*
 @RibbonClient("COUPON-SERVICE") 
 "COUPON-SERVICE" this is to tell ribbon where the client is running. We will just provide service name
 Ribbon will automatically figure out where the service is running by connecting to Eureka and
 then it will distribute the load across the servers */
+
+
+/*
+ FeignClient("zuul-api-gateway")
+ 
+   wrt Zuul proxy we have added three changes.
+   
+   1. @FeignClient("COUPON-SERVICE")  --changed to -> @FeignClient("zuul-api-gateway")  
+   This is because now the request will first go to ZullProxy server and then it will redired to the actual server.
+   
+   2.value = "/couponapi/coupons/{code}" --changed to-> value = "/COUPON-SERVICE/couponapi/coupons/{code}"
+   This is because sicne we remove the maping in step1, Zull should know in which server to
+   hit the request. So, we add as prefix, the spring.application.name of that server in url
+   
+   3. We removed client side Load Balancing i.e @RibbonClient("COUPON-SERVICE")
+   This is because Zull will do the server side Load balancing automatically
+   So we dont need client side load balancing
+   
+   Internally Zuul also users ribbon-eureka to do server side load balancing
+   
+ 
+*/
